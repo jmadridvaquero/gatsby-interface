208ef911325de8b76cc27ab7c1fd8525a1db5b7e
Younes <mia.san.younes@gmail.com>
2020-01-22 18:04:25 -0500

    both chevrons work on carousel

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Carousel from 'nuka-carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
 
 class Pricing extends React.Component {
   render() {
     return (
       <Carousel renderCenterLeftControls={({ previousSlide }) => (
         <FontAwesomeIcon icon={faChevronLeft} onClick={previousSlide}/>
-      )}>
+      )}
+      renderCenterRightControls={({ nextSlide }) => (
+        <FontAwesomeIcon icon={faChevronRight} onClick={nextSlide}/>
+      )}
+      >
         <p>bhsdughkd</p>
         <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide2" />
         <img src="https://via.placeholder.com/400/ffffff/c0392b/&text=slide3" />
