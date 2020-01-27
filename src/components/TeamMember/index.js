ae91eac6f9e81f881af40b3d42895e0e27c9cf3c
Younes <mia.san.younes@gmail.com>
2020-01-26 20:41:16 -0500

    link works

--- src/components/TeamMember/index.js
+++ src/components/TeamMember/index.js
@@ -1,23 +1,30 @@
 import React from 'react'
+import { Link, graphql, StaticQuery } from 'gatsby'
+
 import PropTypes from 'prop-types'
 
-const TeamMember = (props) => (
-  <div className="col-6 col-lg-3 teamMembers mr-2 mb-2">     
-  <div className='justify-content-center d-flex'>
-    <img src={props.img}/>
-  </div>
-    <div className='justify-content-center d-flex'>
-      <h1 className='text-align-center'>
-        {props.name}
-        </h1>
-    </div>
+const TeamMember = (props) => {
+  
+  return(
+    <Link to="/blog">
+      <div className="col-6 col-lg-3 teamMembers mr-2 mb-2">     
+      <div className='justify-content-center d-flex'>
+        <img src={props.img}/>
+      </div>
+        <div className='justify-content-center d-flex'>
+          <h1 className='text-align-center'>
+            {props.name}
+            </h1>
+        </div>
 
-  <div className='justify-content-center d-flex'>
-    <p className='text-align-center'>
-    {props.title}
-    </p></div>       
-</div>
-)
+      <div className='justify-content-center d-flex'>
+        <p className='text-align-center'>
+        {props.title}
+        </p></div>       
+    </div>    
+    </Link>
+  )
+}
 
 // Testimonials.propTypes = {
 //   testimonials: PropTypes.arrayOf(
